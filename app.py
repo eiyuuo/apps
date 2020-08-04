0f44d956d78e5e3a7bd7547c57e64154d52d787d
Yuto Eimon <e185733@ie.u-ryukyu.ac.jp>
2020-08-04 11:39:57 +0900

    commit

---
+++ app.py
@@ -0,0 +1,96 @@
+from flask import Flask, request, render_template, redirect, url_for, session
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_dance.contrib.github import make_github_blueprint, github
+
+import os
+
+#Oauth
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thisissupposedtosecret'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+github_blueprint = make_github_blueprint(client_id='2373ec8245407c5f6bce', client_secret='50c01537b217705e3162d36b32cfb0e91e5d8409')
+
+app.register_blueprint(github_blueprint, url_prefix='/github_login')
+
+#db
+db_uri = os.environ.get('DATABASE_URL') #or "postgresql://localhost/flasknote"
+app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+db = SQLAlchemy(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    pub_date = db.Column(db.DateTime, nullable=False,
+                                default=datetime.utcnow)
+    name = db.Column(db.String(80))
+    #name = db.Column(db.Text(80))
+    article = db.Column(db.Text())
+    thread_id = db.Column(db.Integer, db.ForeignKey('thread.id'), nullable=False)
+
+    def __init__(self, pub_date, name, article, thread_id):
+        self.pub_date = pub_date
+        self.name = name
+        self.article = article
+        self.thread_id = thread_id
+
+class Thread(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    threadname = db.Column(db.String(80), unique=True)
+    articles = db.relationship('Article', backref='thread', lazy=True)
+
+    def __init__(self, threadname, articles=[]):
+        self.threadname = threadname
+        self.articles = articles
+
+@app.route("/")
+def main():
+    threads = Thread.query.all()heroku addons:add heroku-postgresql
+    return render_template("index.html", threads=threads)
+
+@app.route("/thread", methods=["POST"])
+def thread():
+    thread_get = request.form["thread"]
+    threads = Thread.query.all()
+    thread_list = []
+    threads = Thread.query.all()
+    for th in threads:
+        thread_list.append(th.threadname)
+    if thread_get in thread_list:
+        thread = Thread.query.filter_by(threadname=thread_get).first()
+        articles = Article.query.filter_by(thread_id=thread.id).all()
+        return render_template("thread.html",
+                                articles=articles,
+                                thread=thread_get)
+    else:
+        thread_new = Thread(thread_get)
+        db.session.add(thread_new)
+        db.session.commit()
+        articles = Article.query.filter_by(thread_id=thread_new.id).all()
+        return render_template("thread.html",
+                                articles=articles,
+                                thread=thread_get)
+
+@app.route("/result", methods=["POST"])
+def result():
+    date = datetime.now()
+    article = request.form["article"]
+    name = request.form["name"]
+    thread = request.form["thread"]
+    thread = Thread.query.filter_by(threadname=thread).first()
+    admin = Article(pub_date=date, name=name, article=article, thread_id=thread.id)
+    db.session.add(admin)
+    db.session.commit()
+    return render_template("bbs_result.html", article=article, name=name, now=date)
+
+@app.route("/github")
+def github_login():
+    if not github.authorized:
+        return redirect(url_for('github.login'))
+    account_info = github.get('/user')
+    if account_info.ok:
+        account_info_json = account_info.json()
+        return '<h1>Your Github name is {}</h1>'.format(account_info_json['login'])
+    return 'Request faild!</h1>'
+
+if __name__ == "__main__":
+    app.run(debug=True)
